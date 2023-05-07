291488883b366bdcce472bc11efd3802ea06420e
Shruti-Koli <koli.shruti55@gmail.com>
2023-05-07 23:50:36 +0530

    task9

--- filex.js
+++ filex.js
@@ -1,16 +1,21 @@
 var form = document.getElementById("addForm");
 var itemlist = document.getElementById("items");
+var filter = document.getElementById("filter");
 
 form.addEventListener("submit",additem);
 itemlist.addEventListener("click",remove);
+filter.addEventListener("keyup",filterItems);
 
 function additem(e){
     e.preventDefault();
     var newItem= document.getElementById("item");
-
+    var descr = document.getElementById("description");
+    
     var li = document.createElement("li");
     li.className="list-group-item";
+    
     li.appendChild(document.createTextNode(newItem.value))
+    li.appendChild(document.createTextNode(descr.value))
     //console.log(li);
 
     //create delete button 
@@ -37,4 +42,24 @@ function remove(e){
             itemlist.removeChild(todel);
         }
     }
+}
+
+function filterItems(e){
+    //convert to lower case
+    var text = e.target.value.toLowerCase();
+    var ilist = document.getElementsByTagName("li");
+    //console.log(ilist[0].childNodes[0])
+    //convert to array and then for
+    Array.from(ilist).forEach(function(i){
+        var itemname = i.childNodes[0].textContent;
+        var itemname2 = i.childNodes[1].textContent;
+        console.log(itemname);
+
+        if((itemname.toLowerCase().indexOf(text) != -1)  || (itemname2.toLowerCase().indexOf(text) != -1)){
+            i.style.display="block";
+        }
+        else{
+            i.style.display="none";
+        }
+    });
 }
