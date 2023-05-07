55f51bec00a7eebdb35e83c3f3af0ccc7c63d191
Shruti-Koli <koli.shruti55@gmail.com>
2023-05-07 17:05:54 +0530

    task4

--- dom.js
+++ dom.js
@@ -4,4 +4,13 @@ header1.style.borderBottom="3px solid black";
 var addItem = document.getElementsByClassName("title");
 console.log(addItem[0]);
 addItem[0].style.color="green";
-addItem[0].style.fontWeight="bold";
+addItem[0].style.fontWeight="bold"; 
+
+// Make the 3 rd element in the list have green background color
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor="green";
+
+// Make all the elements in the list have bold color font
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+}
