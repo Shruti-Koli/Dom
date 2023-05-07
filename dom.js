79fddfd003519cf56492bab0c41ad8e874b2b35a
Shruti-Koli <koli.shruti55@gmail.com>
2023-05-07 16:00:01 +0530

    new

---
+++ dom.js
@@ -0,0 +1,7 @@
+var header1 = document.getElementById('header-title');
+console.log(header1);
+header1.style.borderBottom="3px solid black";
+var addItem = document.getElementsByClassName("title");
+console.log(addItem[0]);
+addItem[0].style.color="green";
+addItem[0].style.fontWeight="bold";
